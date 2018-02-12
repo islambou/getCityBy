e5d80238dea3215a6806ea0714f022f9a5e30ad6
islambou <islambou@gmail.com>
2018-02-12 18:12:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using france_cities.tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace france_cities
+{
+    class Program
+    {
+
+        enum Cre { Département = 1, Slug, Nom, Nom_simple, Nom_reel, Nom_soundex, Nom_metaphone,
+        Code_postal, Numéro_de_commune, Code_commune, Arrondissement, Canton, Population_en_2010 ,
+        Population_en_1999, Population_en_2012, Densité_en_2010, Surface, Longitude_latitude_en_degré,
+        Longitude_latitude_en_GRD, Longitude_latitude_en_DMS, Altitude_minimale_maximale
+        }; 
+
+        static void Main(string[] args)
+        {
+            howto();
+            String file_location = @"C:\fv.csv";
+
+            
+
+            citiesManager CM = new citiesManager(file_location);
+
+            while(true){
+                Console.WriteLine("********************************");
+                Console.WriteLine("enter criteria");
+                int c = int.Parse(Console.ReadLine()); 
+
+                Console.WriteLine("enter value");
+                String v = Console.ReadLine();
+
+                Console.WriteLine(CM.getCities(v, c));
+            }
+
+            
+
+        
+        }
+
+        static void howto()
+        {
+            Console.WriteLine("*** the file should be like this : c:\\fv.csv ***");
+            Console.WriteLine("*** Search cretearia ***");
+            foreach (Cre val in Enum.GetValues(typeof(Cre)))
+            {
+                Console.WriteLine((int)val + " : " + val);
+            }
+            
+            
+        }    
+
+        }
+    }
+ 
---
+++ tools/citiesManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+// compile with: /doc:france_cities.xml 
+namespace france_cities.tools
+{
+    class citiesManager
+    {
+        private String DataSource;
+        private Char DataSeparator = ',';
+        private Char mKeysSplitter = '-';
+        private int CityNameColumn = 3;
+        private int PostCodeColumn = 8;
+
+   
+
+        public citiesManager(String s)
+        {
+            ///<param name="s">this parameter is to set the data source file (cvs file)</param>   
+            this.DataSource = s;
+        }
+     
+        
+        public void setDataSource(String s)
+        {
+            this.DataSource = s;
+        }
+        public void setDataSeparator(char s)
+        {
+            this.DataSeparator = s;
+        }
+        public void setmKeysSplitter(char s)
+        {
+            this.mKeysSplitter = s;
+        }
+
+
+        private Hashtable dataToHashtable(int column)
+        {
+            /// <summary>dataToHashtable is simply a methode
+            /// that gets data from the source (cvs) and turns it
+            /// into a hashtable (key : depends on our query , value: city name)
+            /// </summary>
+
+
+            Hashtable result = new Hashtable();
+            try {
+                using (StreamReader reader = new StreamReader(DataSource))
+            {
+                
+                while (!reader.EndOfStream)
+                {
+                    String row = reader.ReadLine();
+                    String[] values = row.Split(DataSeparator);
+
+                    //this line takes too much time but it is necessary 
+                    values[column] = values[column].Replace("\"","");
+
+
+                    //this line was added just because one city can have more than one post code , like paris
+                    String[] multiple_keys = column == PostCodeColumn ? values[column].Split(mKeysSplitter) : new[] { values[column] };
+
+                    foreach (String key in multiple_keys) {
+
+                        //populating our hashtable 
+                        if (!result.ContainsKey(key))
+                            result.Add(key, values[CityNameColumn]);
+
+                    else
+                            result[key] += "," + values[CityNameColumn];
+                    }
+                }
+            }
+            
+                }
+            catch (Exception)
+            {
+                Console.WriteLine("couldn't parse data to a hashtable");
+            }
+
+            return result;
+
+        }
+
+        public String getCities(String value, int criteria)
+        {
+            /// <param name="value">Used to set the value of our search query  (Ex : 75)</param>
+            /// <param name="criteria">Used to set the criteria of our search query  (Ex : 1 => "departement")</param> 
+
+            Hashtable data = dataToHashtable(criteria);
+            if (data.ContainsKey(value)) return (string)data[value];
+            return "Key not found!";
+        }
+  
+     
+        
+        
+        
+        
+        public String getCitiesByDepartement(String d)
+        {
+            return getCities(d, 1);
+        }      
+        public String getCityByPostCode(String d)
+        {
+            return getCities(d, 8);
+        }
+     
+
+        }
+}
